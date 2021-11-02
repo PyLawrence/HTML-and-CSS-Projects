78a0790a451ad7a5820ce43217d02b9a7c74e1e5
PyLawrence <89617029+PyLawrence@users.noreply.github.com>
2021-11-01 20:06:37 -0400

    Added lightbox modal for computer images

---
+++ One-Page Website/JS/one_page_website.js
@@ -0,0 +1,18 @@
+function openModal(selected) {
+    document.getElementById("myModal").style.display = "block";
+
+    let elements = document.getElementsByClassName("mySlides");
+
+    for (let j = 0; j <= elements.length; j++) {
+        if (elements[j].id.slice(-1) == selected) {
+            elements[j].style.display = "block";
+        }
+        else {
+            elements[j].style.display = "none";
+        }
+    }
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
